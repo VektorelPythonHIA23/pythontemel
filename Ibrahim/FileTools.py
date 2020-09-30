4553086f342b85fd8a758954a24865ceac9a13ca
vektorelpython23_VektorelAnaBina <vektorelpython23@gmail.com>
2020-09-30 21:08:45 +0300

    30092020

--- Ibrahim/FileTools.py
+++ Ibrahim/FileTools.py
@@ -25,4 +25,34 @@ class FileTool:
             liste.append(input(f"{item}"))
         return ";".join(liste) + "\n"
 
-     
+    def ekleme(self):
+        self.kayitlar.append(self.girisYap())
+
+    def silme(self):
+        listeleme()
+        del self.kayitlar[int(input("Silmek istediğiniz kaydı seçiniz"))-1] 
+
+    def guncelle(self):
+        listeleme()
+        self.kayitlar[int(input("Güncellemek istediğiniz\
+         kaydı seçiniz:"))-1] = self.girisYap()
+
+    def menu(self):
+        menu = """
+        1-Ekleme
+        2-Güncelleme
+        3-Silme
+        4-Listeleme
+        5-Çıkış
+        İşlem Seçiniz:
+        """
+        fonkListesi=["",self.ekleme,self.gunceleme,self.silme,self.listeleme]
+        anahtar = 1
+        while anahtar == 1:
+            islem = int(input(menu))
+            if islem != 5:
+                fonkListesi[islem]()
+            else:
+                anahtar = 0
+        
+
