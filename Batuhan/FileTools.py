10fcea658573afa0706ba84166d9f47023e21070
Batuhan Arslanoğlu <batuhanarslanoglu23@gmail.com>
2020-09-30 21:23:09 +0300

    aa
    aa

--- Batuhan/FileTools.py
+++ Batuhan/FileTools.py
@@ -1,41 +1,44 @@
-class FileTool :
+class FileTool:
     ext = ".csv"
-    def __init__(self,adresi,alanlar):
+    def __init__(self,adres,alanlar):
         self.adres = adres + FileTool.ext
         self.dosya = self.dosyaAc()
-        self.kayitlar = self.dosya.readline()
+        self.kayitlar = self.dosya.readlines()
         self.alanlar = alanlar
     def dosyaAc(self):
         import os
-        if os.path.exists(self.adres) :
+        if os.path.exists(self.adres):
             return open(self.adres,"r+")
         else:
             return open(self.adres,"w+")
     
-    def listeleme(self):
+
+    def listeleme(self): #TODO İ DÜZELTİLECEK
         i = 1
         for item in self.kayitlar:
             print(f"{i} -",*item.split[";"],end="")
             i+=1
+    
     def girisYap(self):
         liste = []
         for item in self.alanlar:
             liste.append(input(f"{item}"))
-        return ";".join(liste) + "\n"          
-    
+        return ";".join(liste) + "\n"
+
     def ekleme(self):
         self.kayitlar.append(self.girisYap())
-    
+
     def silme(self):
-        listeleme()
-        del self.kayitlar[int(input("Silmek istediğiniz kaydı seçiniz: "))-1]
-    
+        self.listeleme()
+        del self.kayitlar[int(input("Silmek istediğiniz kaydı seçiniz"))-1] 
+
     def guncelle(self):
-        listelemele()
-        self.kayitlar[int(input("Güncellemek isteğiniz kaydı seçiniz: "))-1] = self.girisYap()
-    
+        self.listeleme()
+        self.kayitlar[int(input("Güncellemek istediğiniz\
+         kaydı seçiniz:"))-1] = self.girisYap()
+
     def menu(self):
-    menu = """
+        menu = """
         1-Ekleme
         2-Güncelleme
         3-Silme
@@ -43,20 +46,27 @@ class FileTool :
         5-Çıkış
         İşlem Seçiniz:
         """
-    fonkListesi = ["",self.ekleme,self.guncelleme,self.silme,self.listeleme]
-    anahtar = 1
-    while anahtar == 1 :
+        fonkListesi=["",self.ekleme,self.guncelle,self.silme,self.listeleme]
+        anahtar = 1
+        while anahtar == 1:
             islem = int(input(menu))
-            if islem != 5 :
+            if islem != 5:
                 fonkListesi[islem]()
             else:
                 anahtar = 0
-    else:
+        else:
             self.dosya.seek(0)
             self.dosya.truncate()
             self.dosya.writelines(self.kayitlar)
-            self.flush()
+            self.dosya.flush()
+
 
     def __del__(self):
+        print("RIP")
         self.dosya.seek(0)
-        
+        self.dosya.truncate()
+        self.dosya.writelines(self.kayitlar)
+        self.dosya.close()
+
+nesne = FileTool("defter",["Adı","Soyadı","Tel"])
+nesne.menu()
