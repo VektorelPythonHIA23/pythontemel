2e196b01a1689088a9fb10ddbb242ff358171504
Bulent Ozturk <blozturk76@gmail.com>
2020-09-30 21:07:33 +0300

    n
    n

--- Bulent/fileTools.py
+++ Bulent/fileTools.py
@@ -25,3 +25,31 @@ class fileTool:
         for item in self.alanlar:
             liste.append(input(f"{item}"))
         return ";".join(liste) + "\n"
+
+    def ekleme(self):
+        self.kayitlar.append(self.girisYap())
+
+    def silme(self):
+        listeleme()
+        del self.kayitlar[int(input("Silmek istediğiniz kaydı seçiniz"))-1]
+
+    def guncelle(self):
+        listeleme()
+        self.kayitlar[int(input("Güncelleme istediğiniz kaydı seçiniz"))-1] = self.girisYap()
+
+    def menu(self):
+        menu = """
+        1-Ekleme
+        2-Güncelleme
+        3-Silme
+        4-Listeleme
+        5-Çıkış
+        İşlem Seçiniz:
+        """
+
+        fon
+
+        anahtar = 1
+        while anahtar == 1:
+            islem = int(input(menu))
+            if islem != 5:
