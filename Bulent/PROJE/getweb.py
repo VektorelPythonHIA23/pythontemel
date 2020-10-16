935a461413a8cc44261e3ede5ad4976d829f8ae5
blozturk76 <72154284+blozturk76@users.noreply.github.com>
2020-10-16 21:18:22 +0300

    bulent-webget
    test sdf

--- Bulent/PROJE/getweb.py
+++ Bulent/PROJE/getweb.py
@@ -1,11 +1,18 @@
 import requests
 from bs4 import BeautifulSoup as BSP
 import time
+headers = {'user-agent': 'my-app/0.0.1'}
 
-r = requests.get("https://www.nasa.gov/centers/armstrong/home/index.html")
+sayfa = requests.get("https://www.sahibinden.com/kiralik-daire/ankara-cankaya",timeout=100,headers=headers)
 time.sleep(3)
 
-kaynak = BSP(r.content,"lxml")
-kaynak.
-print(kaynak.title)
-print(kaynak.find("div").text)
+soup = BSP(sayfa.content,"html.parser")
+
+# liste1= soup.find_all("div")
+liste1 = soup.findAll("div",class_="action-wrapper")
+for s in liste1:
+    print ''.join(s['class']).strip(), s.text
+#liste1= soup.findAll("td",class_="searchResultsAttributeValue")
+
+#print(len(liste1))
+#print(liste1)
