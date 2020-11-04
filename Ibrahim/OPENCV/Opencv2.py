b79f2ad84e7304bdc45d67cca0b059044fc83dcb
vektorelpython23 <72040541+vektorelpython23@users.noreply.github.com>
2020-11-04 18:43:37 +0200

    04112020

--- Ibrahim/OPENCV/Opencv2.py
+++ Ibrahim/OPENCV/Opencv2.py
@@ -7,7 +7,8 @@ while True:
     gri = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gri,(15,15),0)
     cv2.imshow("Kamera",blur)
-
+    # frame[150:250,150:250] = frame[50:150,50:150]
+    # cv2.imshow("Kamera3",frame)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
