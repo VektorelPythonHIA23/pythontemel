87e6b645af2c70c39eb0768d479f7619688103bb
vektorelpython23 <72040541+vektorelpython23@users.noreply.github.com>
2020-11-16 20:15:16 +0200

    16112020_3

--- Ibrahim/BOT/instabot.py
+++ Ibrahim/BOT/instabot.py
@@ -28,11 +28,12 @@ class InstaBot():
         devam = self.browser.find_element_by_xpath(r'//*[@id="react-root"]/section/main/div/div/div/div/button')
         devam.click()
         try:
-            devam = self.browser.find_element_by_css_selector("button.aOOlW:nth-child(1)")
+            devam = self.browser.find_element_by_css_selector("button.aOOlW:nth-child(2)")
             devam.click()
         except:
             pass
         self.beklet()
+
     def takipEt(self,username):
         self.browser.get(rf"https://www.instagram.com/{username}/")
         self.beklet()
@@ -44,6 +45,44 @@ class InstaBot():
             print("Zaten Takipteyiz")
 
 
-bot1 = InstaBot("vektorelpython23","vektorel2004",10)
+    def takipBırak(self,username):
+        self.browser.get(rf"https://www.instagram.com/{username}/")
+        self.beklet()
+        takip = self.browser.find_element_by_css_selector("button._5f5mN:nth-child(1)")
+        if takip.text == "":
+            takip.click()
+            time.sleep(2)
+            takipbirak = self.browser.find_element_by_css_selector("button.aOOlW:nth-child(1)")
+            takipbirak.click()
+        else:
+            
+            print("Zaten Takip etmiyoruz")
+
+
+    def ilkFotoLike(self,username):
+        # div.v1Nh3:nth-child(1) > a:nth-child(1)
+        self.browser.get(rf"https://www.instagram.com/{username}/")
+        time.sleep(4)
+        self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        like1 = self.browser.find_element_by_css_selector("div.v1Nh3:nth-child(1)")
+        like1.click()
+        time.sleep(4)
+        like2 = self.browser.find_element_by_css_selector(".fr66n > button:nth-child(1)")
+        like2.click()
+        time.sleep(4)
+        
+
+
+    
+
+bot1 = InstaBot("vektorelpython23","vektorel2004",5)
 bot1.girisYap()
-bot1.takipEt("codumpython")
+# bot1.takipEt("codumpython")
+bot1.ilkFotoLike("codumpython")
+
+
+# bot1.takipBırak("codumpython")
+
+
+
+# https://github.com/yogeshwaran01/Python-Scripts/blob/master/Scripts/instagramy-docs.ipynb
