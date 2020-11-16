6957621f26f30810bb8632ddf9f22f2ee3e2f208
eebrusolak <70767204+eebrusolak@users.noreply.github.com>
2020-11-16 18:44:13 +0300

    12.11.20

--- Ebru/Opencv4.py
+++ Ebru/Opencv4.py
@@ -2,10 +2,10 @@
 import cv2
 import numpy as np
 
-img = cv2.imread(r"Ibrahim\OPENCV\bookpage.jpg")
-
-retval,threshold = cv2.thresold(img,10,255,cv2.THRESH_BINARY)
-
+img = cv2.imread(r"Ebru\OPENCV\bookpage.jpg")
+gri = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# retval,threshold = cv2.thresold(img,10,255,cv2.THRESH_BINARY)
+th = cv2.adaptiveThreshold(gri,255,ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,155,1)
 
 cv2.imshow("Sonuc",img)
 cv2.imshow("Sonuc2",threshold)
