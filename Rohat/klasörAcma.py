188b032a2b10819e0d51c67b0d82f21d186c7539
gedemen <rohatergen.62@gmail.com>
2021-01-05 23:26:33 +0300

    RohatErgen_Proje
    Projem(YüzTanıma)

--- Rohat/klasörAcma.py
+++
@@ -1,5 +0,0 @@
-# import os
-# print(os.getcwd())
-# liste = ["Bulent","MEmin","Rohat","Abuzer","Batuhan","Deniz","Ebru","Eren","Hamit","MNurhan","MTamer","SuatF"]
-# for item in liste:
-#     os.mkdir(item)
